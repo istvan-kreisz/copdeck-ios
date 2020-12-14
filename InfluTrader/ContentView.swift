68eaf583ea2e1dd42199c9b76f65d42881f3f0b5
kreiszistvan <kreiszdev@gmail.com>
2020-12-13 22:30:38 -0500

    first commit

---
+++ InfluTrader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfluTrader
+//
+//  Created by István Kreisz on 12/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
