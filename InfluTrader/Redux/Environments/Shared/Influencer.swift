861e821a05f77fcb481761ed54cacdfa17ad81f4
kreiszistvan <kreiszdev@gmail.com>
2021-01-28 19:21:16 -0500

    yo

---
+++ InfluTrader/Redux/Environments/Shared/Influencer.swift
@@ -0,0 +1,18 @@
+//
+//  Influencer.swift
+//  InfluTrader
+//
+//  Created by István Kreisz on 12/14/20.
+//
+
+import Foundation
+
+struct Influencer {
+    let name: String
+    let id: String
+    let statusChange: Double
+    let price: Double
+    let availableShares: Int
+    let youtubeFollowers: Int
+    let priceHistory: [Double]
+}
---
+++ InfluTrader/Redux/Environments/Shared/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  InfluTrader
+//
+//  Created by István Kreisz on 12/14/20.
+//
+
+import Foundation
+
+struct Player {
+    struct Asset {
+        let
+    }
+    let bankHistory: [Double]
+    let percentile: Int
+    let portfolio: [(Influencer, Int)]
+}
