df421eb3dd9f80ed0df1a91821bb04cbdbb87634
kreiszistvan <kreiszdev@gmail.com>
2021-02-04 00:54:26 -0500

    work on ui

--- InfluTrader/Screens/Main/Home/HomeView.swift
+++ InfluTrader/Screens/Main/Home/HomeView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var store: Store<MainState, MainAction, Main>
 
+    let colors: [Color] = [.red, .yellow, .green, .purple, .orange]
+
+    @State var selectedStock: Stock?
+
     func ownedAmount(of stock: Stock) -> Int {
         store.state.user?.transactions
             .first(where: { $0.id == stock.id })
@@ -17,44 +21,53 @@ struct HomeView: View {
     }
 
     var body: some View {
-        VStack {
-            Text(store.state.user?.name ?? "")
-//            ScrollView {
-//                HStack {
-//                    ForEach(store.state.trendingStocks ?? []) { stock in
-//                        VStack {
-//                            ZStack {
-//                                Circle()
-//                                    .frame(width: 80, height: 80)
-//                                Image("profile")
-//                                    .frame(width: 60, height: 60)
-//                                    .cornerRadius(30)
-//                            }
-//                            Text(stock.id)
-//                        }
-//                        .padding()
-//                    }
-//                }
-//            }
-            List {
-                ForEach(store.state.userStocks ?? []) { stock in
+        ZStack {
+            VStack {
+                Text(store.state.user?.name ?? "")
+                ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
-                        Text(stock.id)
-                        Text("\(ownedAmount(of: stock))")
-                        Text("\(stock.price)")
-                        Button("Buy 1") {
-                            store.send(.tradeStock(stockId: stock.id, amount: 1, type: .buy))
-                        }
-                        Button("Sell 1") {
-                            store.send(.tradeStock(stockId: stock.id, amount: 1, type: .sell))
+                        ForEach(store.state.trendingStocks ?? []) { stock in
+                            VStack {
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 80, height: 80)
+                                        .foregroundColor(colors.randomElement()!)
+                                    Image("profile")
+                                        .frame(width: 60, height: 60)
+                                        .cornerRadius(30)
+                                }
+                                Text(stock.id)
+                            }
+                            .padding()
+                            .onTapGesture {
+                                selectedStock = stock
+                            }
                         }
                     }
-                    .buttonStyle(PlainButtonStyle())
+                }
+                .frame(height: 100)
+                List {
+                    ForEach(store.state.userStocks ?? []) { stock in
+                        HStack {
+                            Text(stock.id)
+                            Text("\(ownedAmount(of: stock))")
+                            Text("\(stock.price)")
+                            Button("Buy 1") {
+                                store.send(.tradeStock(stockId: stock.id, amount: 1, type: .buy))
+                            }
+                            Button("Sell 1") {
+                                store.send(.tradeStock(stockId: stock.id, amount: 1, type: .sell))
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
                 }
             }
-        }
-        .onAppear {
-            print(store.state.user?.name ?? "")
+            if selectedStock != nil {
+                StockView(stock: $selectedStock)
+                    .transition(.scale)
+                    .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            }
         }
     }
 }
@@ -62,5 +75,26 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+
+struct StockView: View {
+
+    @Binding var stock: Stock?
+
+    var body: some View {
+        VStack {
+            Text(stock?.id ?? "nah")
+            Button(action: {
+                self.stock = nil
+            }) {
+                Text("Click me")
+            }
+            .frame(width: 30, height: 30)
+        }
+        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+        .background(Color.green)
+        .edgesIgnoringSafeArea(.all)
     }
 }
--- InfluTrader/Screens/Main/MainView.swift
+++ InfluTrader/Screens/Main/MainView.swift
@@ -12,23 +12,25 @@ struct MainView: View {
     @EnvironmentObject var store: Store<MainState, MainAction, Main>
 
     var body: some View {
-        TabView {
-            NavigationView {
-                HomeView()
-            }.tabItem {
-                Image(systemName: "heart.fill")
-                    .imageScale(.large)
-                Text("today")
-            }
+//        TabView {
+//            NavigationView {
+//                HomeView()
+//            }.tabItem {
+//                Image(systemName: "heart.fill")
+//                    .imageScale(.large)
+//                Text("today")
+//            }
+//
+//            NavigationView {
+//                Text("Second view")
+//            }.tabItem {
+//                Image(systemName: "chevron.up.circle.fill")
+//                    .imageScale(.large)
+//                Text("trends")
+//            }
+//        }
+        HomeView()
 
-            NavigationView {
-                Text("Second view")
-            }.tabItem {
-                Image(systemName: "chevron.up.circle.fill")
-                    .imageScale(.large)
-                Text("trends")
-            }
-        }
         .onAppear {
             store.send(.getMainFeedData)
         }
