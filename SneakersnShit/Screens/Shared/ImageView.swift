b8911fff501248237a64590a2779d45f6b293da4
István Kreisz <istvankreisz@Istvans-MacBook-Pro-2.local>
2021-07-26 00:29:35 +0200

    added nuke + refactored imageview

--- SneakersnShit/Screens/Shared/ImageView.swift
+++ SneakersnShit/Screens/Shared/ImageView.swift
@@ -6,31 +6,9 @@
 //
 
 import SwiftUI
-import Combine
-
-class ImageLoader: ObservableObject {
-    var didChange = PassthroughSubject<Data, Never>()
-    var data = Data() {
-        didSet {
-            didChange.send(data)
-        }
-    }
-
-    init(urlString: String) {
-        guard let url = URL(string: urlString), !urlString.isEmpty else { return }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
-            DispatchQueue.main.async {
-                self.data = data
-            }
-        }
-        task.resume()
-    }
-}
+import NukeUI
 
 struct ImageView: View {
-    @ObservedObject var imageLoader: ImageLoader
-    @State var image: UIImage = UIImage()
     let url: String
     let size: CGFloat
     let aspectRatio: CGFloat?
@@ -39,7 +17,6 @@ struct ImageView: View {
         self.url = url
         self.size = size
         self.aspectRatio = aspectRatio
-        self.imageLoader = ImageLoader(urlString: url)
     }
 
     var body: some View {
@@ -49,26 +26,13 @@ struct ImageView: View {
                     .resizable()
                     .aspectRatio(aspectRatio, contentMode: .fit)
                     .frame(width: size, height: size)
-                    .onReceive(imageLoader.didChange) { data in
-                        self.image = UIImage(data: data) ?? UIImage()
-                    }
             } else {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(aspectRatio, contentMode: .fit)
+                LazyImage(source: url, resizingMode: .aspectFit)
                     .frame(width: size, height: size)
-                    .onReceive(imageLoader.didChange) { data in
-                        self.image = UIImage(data: data) ?? UIImage()
-                    }
             }
         #else
-            Image(uiImage: image)
-                .resizable()
-                .aspectRatio(aspectRatio, contentMode: .fit)
+            LazyImage(source: url, resizingMode: .aspectFit)
                 .frame(width: size, height: size)
-                .onReceive(imageLoader.didChange) { data in
-                    self.image = UIImage(data: data) ?? UIImage()
-                }
         #endif
     }
 }
