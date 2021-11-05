b3f411732f9d8f8f8097177f23390ab23f095ce1
Istvan Kreisz <kreiszdev@gmail.com>
2021-11-05 15:26:48 -0500

    removed facebook sdk

--- SneakersnShit/Services/PushNotificationService/PushNotificationService.swift
+++ SneakersnShit/Services/PushNotificationService/PushNotificationService.swift
@@ -13,6 +13,8 @@ class PushNotificationService: NSObject {
     private let gcmMessageIDKey = "gcm.message_id"
     private var userId: String?
     private let topics = ["all", "alliOS"]
+    
+    let latestMessageSubject = CurrentValueSubject<(messageId: String, channelId: String)?, Never>(nil)
 
     var deviceId: String {
         UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString
@@ -32,9 +34,6 @@ class PushNotificationService: NSObject {
         if let messageID = userInfo[gcmMessageIDKey] {
             print("Message ID: \(messageID)")
         }
-
-        // Print full message.
-        print(userInfo)
 
         #warning("fetch messages & navigate to messages tab")
 
@@ -130,46 +129,22 @@ class PushNotificationService: NSObject {
             }
         }
     }
-//
-//    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-//        // If you are receiving a notification message while your app is in the background,
-//        // this callback will not be fired till the user taps on the notification launching the application.
-//        // TODO: Handle data of notification
-//
-//        // With swizzling disabled you must let Messaging know about the message, for Analytics
-//        // Messaging.messaging().appDidReceiveMessage(userInfo)
-//
-//        // Print message ID.
-//        if let messageID = userInfo[gcmMessageIDKey] {
-//            print("Message ID: \(messageID)")
-//        }
-//
-//        // Print full message.
-//        print(userInfo)
-//    }
 }
 
 extension PushNotificationService: UNUserNotificationCenterDelegate {
     #warning("do we need to implement this? - decide how to present")
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification,
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         let userInfo = notification.request.content.userInfo
 
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-        // [START_EXCLUDE]
-        // Print message ID.
+        print(userInfo)
         if let messageID = userInfo[gcmMessageIDKey] {
             print("Message ID: \(messageID)")
+            completionHandler([[.sound, .badge]])
+        } else {
+            completionHandler([[.sound, .badge]])
         }
-        // [END_EXCLUDE]
-
-        // Print full message.
-        print(userInfo)
-
-        // Change this to your preferred presentation option
-        completionHandler([[.alert, .sound]])
     }
 
     #warning("do we need to implement this? - react to user interaction")
@@ -181,16 +156,11 @@ extension PushNotificationService: UNUserNotificationCenterDelegate {
         // Print message ID.
         if let messageID = userInfo[gcmMessageIDKey] {
             print("Message ID: \(messageID)")
+            completionHandler([[.sound, .badge]])
         }
-        // [END_EXCLUDE]
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-        // Print full message.
         print(userInfo)
 
-        completionHandler()
+
     }
 }
 
