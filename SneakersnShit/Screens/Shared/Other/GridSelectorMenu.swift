48ca3d55bfdc9ec83225d3a56bd20e96c23b4597
Istvan Kreisz <kreiszdev@gmail.com>
2021-11-09 15:09:03 -0500

    grid selector updates

--- SneakersnShit/Screens/Shared/Other/GridSelectorMenu.swift
+++ SneakersnShit/Screens/Shared/Other/GridSelectorMenu.swift
@@ -12,66 +12,34 @@ struct GridSelectorMenu: View {
     enum Style {
         case gray, white
     }
+    static let cornerRadius: CGFloat = 7
+    static let itemWidth: CGFloat = 47
 
     @Binding var selectedItem: String
     var options: [String]
     var style: Style = .gray
 
     var items: [GridItem] {
-        Array(repeating: GridItem(.flexible(minimum: 30)), count: 5)
+        Array(repeating: GridItem(.flexible(minimum: Self.itemWidth)), count: UIScreen.isSmallScreen ? 5 : 6)
     }
 
     var body: some View {
         LazyVGrid(columns: items, spacing: 10) {
             ForEach(options, id: \.self) { option in
                 Text(option)
+                    .font(.semiBold(size: 16))
                     .foregroundColor(Color.customText1)
-                    .frame(width: 40, height: 40)
-                    .background(Color.customAccent1)
-                    .overlay(RoundedRectangle(cornerRadius: 5).stroke(option == selectedItem ? Color.red : Color.clear, lineWidth: 2))
+                    .frame(width: Self.itemWidth, height: Self.itemWidth)
+                    .background(style == .gray ? Color.customAccent4 : Color.customWhite)
+                    .cornerRadius(Self.cornerRadius)
+                    .withDefaultShadow(color: style == .white ? .customAccent3 : .clear)
+                    .overlay(RoundedRectangle(cornerRadius: Self.cornerRadius)
+                        .stroke(option == selectedItem ? Color.customBlue : Color.clear, lineWidth: 2)
+                        .background((option == selectedItem ? Color.customBlue.opacity(0.1) : Color.clear).cornerRadius(Self.cornerRadius)))
                     .onTapGesture {
                         selectedItem = option
                     }
             }
         }
-        .padding(.horizontal)
     }
-
-//    var body: some View {
-//        VStack(alignment: .leading, spacing: 4) {
-//            Text(title)
-//                .font(.regular(size: 12))
-//                .foregroundColor(style == .white ? .customText1 : .customText2)
-//                .padding(.leading, 5)
-//
-//            GeometryReader { geo in
-//                Menu {
-//                    ForEach(options.reversed(), id: \.self) { option in
-//                        Button(option) {
-//                            selectedItem = option
-//                        }
-//                    }
-//                } label: { Text(selectedItem)
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//                    .padding()
-//                }
-//                .padding(.leading, 0)
-//                .pickerStyle(.menu)
-//                .frame(width: geo.frame(in: .local).width)
-//                .foregroundColor(.customText2)
-//                .overlay(Image(systemName: "chevron.down")
-//                    .font(.bold(size: 14))
-//                    .foregroundColor(.customText2)
-//                    .padding(.trailing, 10)
-//                    .centeredVertically()
-//                    .rightAligned()
-//                    .allowsHitTesting(false))
-//                .frame(height: Styles.inputFieldHeight)
-//                .background(style == .gray ? Color.customAccent4 : Color.customWhite)
-//                .cornerRadius(Styles.cornerRadius)
-//            }
-//            .frame(height: Styles.inputFieldHeight)
-//        }
-//        .withDefaultShadow(color: style == .white ? .customAccent3 : .clear)
-//    }
 }
