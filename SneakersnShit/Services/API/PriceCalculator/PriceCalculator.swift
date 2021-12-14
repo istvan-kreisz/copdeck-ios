2162394c0f74182d626c6bfe90ebdba9236e1c1c
Istvan Kreisz <kreiszdev@gmail.com>
2021-12-14 00:06:48 -0500

    feecalculation part 3

--- SneakersnShit/Services/API/PriceCalculator/PriceCalculator.swift
+++ SneakersnShit/Services/API/PriceCalculator/PriceCalculator.swift
@@ -98,69 +98,69 @@ func stockxSellerPrice(price: Double,
 //    const paymentProcessingFeeInUSD = convert(
 //        paymentProcessingFee,
 //        currencyCode,
-//        'USD',
+//        "USD",
 //        false,
 //        exchangeRates
 //    )
 //    if (paymentProcessingFeeInUSD < 3) {
-//        paymentProcessingFee = convert(3, 'USD', currencyCode, false, exchangeRates)
+//        paymentProcessingFee = convert(3, "USD", currencyCode, false, exchangeRates)
 //    } else if (paymentProcessingFeeInUSD > 59.95) {
-//        paymentProcessingFee = convert(59.95, 'USD', currencyCode, false, exchangeRates)
+//        paymentProcessingFee = convert(59.95, "USD", currencyCode, false, exchangeRates)
 //    }
 //    let shippingFee: { fee: number; currency: CurrencyInternal } = { fee: 30, currency: USD }
 //    switch (sellerInfo.countryName) {
-//        case 'Austria':
-//        case 'Belgium':
-//        case 'Bulgaria':
-//        case 'Czech Republic':
-//        case 'Denmark':
-//        case 'Estonia':
-//        case 'Finland':
-//        case 'France':
-//        case 'Germany':
-//        case 'Greece':
-//        case 'Hungary':
-//        case 'Ireland':
-//        case 'Italy':
-//        case 'Lithuania':
-//        case 'Luxembourg':
-//        case 'Netherlands':
-//        case 'Poland':
-//        case 'Portugal':
-//        case 'Romania':
-//        case 'Slovakia':
-//        case 'Slovenia':
-//        case 'Spain':
-//        case 'Sweden':
+//        case "Austria":
+//        case "Belgium":
+//        case "Bulgaria":
+//        case "Czech Republic":
+//        case "Denmark":
+//        case "Estonia":
+//        case "Finland":
+//        case "France":
+//        case "Germany":
+//        case "Greece":
+//        case "Hungary":
+//        case "Ireland":
+//        case "Italy":
+//        case "Lithuania":
+//        case "Luxembourg":
+//        case "Netherlands":
+//        case "Poland":
+//        case "Portugal":
+//        case "Romania":
+//        case "Slovakia":
+//        case "Slovenia":
+//        case "Spain":
+//        case "Sweden":
 //            shippingFee = { fee: 15, currency: EUR }
 //            break
-//        case 'Croatia':
-//        case 'Iceland':
-//        case 'Liechtenstein':
-//        case 'Malta':
-//        case 'Republic of Cyprus':
+//        case "Croatia":
+//        case "Iceland":
+//        case "Liechtenstein":
+//        case "Malta":
+//        case "Republic of Cyprus":
 //            shippingFee = { fee: 25, currency: EUR }
 //            break
-//        case 'Switzerland':
+//        case "Switzerland":
 //            shippingFee = { fee: 20, currency: CHF }
 //            break
-//        case 'UK':
+//        case "UK":
 //            shippingFee = { fee: 13.5, currency: GBP }
 //            break
-//        case 'US (Alaska, Hawaii)':
+//        case "US (Alaska, Hawaii)":
 //            shippingFee = { fee: 25, currency: USD }
 //            break
-//        case 'US (mainland)':
+//        case "US (mainland)":
 //            shippingFee = { fee: 13.95, currency: USD }
 //            break
-//        case 'China':
-//        case 'Singapore':
-//        case 'South Korea':
-//        case 'Taiwan':
+//        case "China":
+//        case "Singapore":
+//        case "South Korea":
+//        case "Taiwan":
 //            shippingFee = { fee: 18, currency: USD }
 //            break
-//        case 'Latvia':
-//        case 'Norway':
+//        case "Latvia":
+//        case "Norway":
 //            shippingFee = { fee: 30, currency: EUR }
 //            break
 //    }
@@ -193,61 +193,61 @@ func klektSellerPrice(price: Double) -> Double {
 //    const group9: [number, number, number] = [35, 30, 40]
 //    const group10: [number, number, number] = [13, 11, 15]
 //    const shippingFees: [CountryName, number, number, number][] = [
-//        ['Austria', ...group1], //
-//        ['Belgium', ...group1], //
-//        ['Bulgaria', ...group1], //
-//        ['Croatia', ...group8], //
-//        ['Republic of Cyprus', ...group1], //
-//        ['Czech Republic', ...group1], //
-//        ['Denmark', ...group1], //
-//        ['Estonia', ...group1], //
-//        ['Finland', ...group1], //
-//        ['France', ...group2], //
-//        ['Germany', ...group2], //
-//        ['Greece', ...group8], //
-//        ['Hungary', ...group1], //
-//        ['Iceland', ...group1], //
-//        ['Ireland', ...group1], //
-//        ['Italy', ...group10], //
-//        ['Latvia', ...group1], //
-//        ['Liechtenstein', ...group1], //
-//        ['Lithuania', ...group1], //
-//        ['Luxembourg', ...group1], //
-//        ['Malta', ...group9], //
-//        ['Netherlands', ...group3], //
-//        ['Norway', ...group1], //
-//        ['Poland', ...group1], //
-//        ['Portugal', ...group1], //
-//        ['Romania', ...group1], //
-//        ['Slovakia', ...group1], //
-//        ['Slovenia', ...group1], //
-//        ['Spain', ...group1], //
-//        ['Sweden', ...group1], //
-//        ['Switzerland', ...group1], //
-//        ['UK', ...group4], //
-//        ['US (mainland)', ...group5], //
-//        ['US (Alaska, Hawaii)', ...group5], //
-//        ['Indonesia', ...group6], //
-//        ['Malaysia', ...group6], //
-//        ['Philippines', ...group6], //
-//        ['Singapore', ...group6], //
-//        ['Thailand', ...group6], //
-//        ['China', ...group6], //
-//        ['South Korea', ...group6], //
-//        ['Taiwan', ...group7],
-//        ['Vietnam', ...group6], //
+//        ["Austria", ...group1], //
+//        ["Belgium", ...group1], //
+//        ["Bulgaria", ...group1], //
+//        ["Croatia", ...group8], //
+//        ["Republic of Cyprus", ...group1], //
+//        ["Czech Republic", ...group1], //
+//        ["Denmark", ...group1], //
+//        ["Estonia", ...group1], //
+//        ["Finland", ...group1], //
+//        ["France", ...group2], //
+//        ["Germany", ...group2], //
+//        ["Greece", ...group8], //
+//        ["Hungary", ...group1], //
+//        ["Iceland", ...group1], //
+//        ["Ireland", ...group1], //
+//        ["Italy", ...group10], //
+//        ["Latvia", ...group1], //
+//        ["Liechtenstein", ...group1], //
+//        ["Lithuania", ...group1], //
+//        ["Luxembourg", ...group1], //
+//        ["Malta", ...group9], //
+//        ["Netherlands", ...group3], //
+//        ["Norway", ...group1], //
+//        ["Poland", ...group1], //
+//        ["Portugal", ...group1], //
+//        ["Romania", ...group1], //
+//        ["Slovakia", ...group1], //
+//        ["Slovenia", ...group1], //
+//        ["Spain", ...group1], //
+//        ["Sweden", ...group1], //
+//        ["Switzerland", ...group1], //
+//        ["UK", ...group4], //
+//        ["US (mainland)", ...group5], //
+//        ["US (Alaska, Hawaii)", ...group5], //
+//        ["Indonesia", ...group6], //
+//        ["Malaysia", ...group6], //
+//        ["Philippines", ...group6], //
+//        ["Singapore", ...group6], //
+//        ["Thailand", ...group6], //
+//        ["China", ...group6], //
+//        ["South Korea", ...group6], //
+//        ["Taiwan", ...group7],
+//        ["Vietnam", ...group6], //
 //    ]
 //    const shippingFee = shippingFees.find((fees) => fees[0] === sellerInfo.countryName)
 //    let fee = 0
 //    if (shippingFee) {
 //        switch (currencyCode) {
-//            case 'EUR':
+//            case "EUR":
 //                fee = shippingFee[1]
 //                break
-//            case 'GBP':
+//            case "GBP":
 //                fee = shippingFee[2]
 //                break
-//            case 'USD':
+//            case "USD":
 //                fee = shippingFee[3]
 //                break
 //            default:
@@ -268,42 +268,42 @@ func klektSellerPrice(price: Double) -> Double {
 //    let sellerFee = 30
 //    let groups: { countries: CountryName[]; fee: number }[] = [
 //        {
-//            countries: ['US (mainland)', 'US (Alaska, Hawaii)', 'Germany', 'UK'],
+//            countries: ["US (mainland)", "US (Alaska, Hawaii)", "Germany", "UK"],
 //            fee: 5,
 //        },
-//        { countries: ['Austria', 'Belgium', 'France', 'Netherlands'], fee: 6 },
+//        { countries: ["Austria", "Belgium", "France", "Netherlands"], fee: 6 },
 //        {
 //            countries: [
-//                'Bulgaria',
-//                'Croatia',
-//                'Republic of Cyprus',
-//                'Czech Republic',
-//                'Estonia',
-//                'Greece',
-//                'Hungary',
-//                'Latvia',
-//                'Lithuania',
-//                'Malta',
-//                'Romania',
-//                'Slovakia',
-//                'Slovenia',
+//                "Bulgaria",
+//                "Croatia",
+//                "Republic of Cyprus",
+//                "Czech Republic",
+//                "Estonia",
+//                "Greece",
+//                "Hungary",
+//                "Latvia",
+//                "Lithuania",
+//                "Malta",
+//                "Romania",
+//                "Slovakia",
+//                "Slovenia",
 //            ],
 //            fee: 24,
 //        },
 //        {
-//            countries: ['China'],
+//            countries: ["China"],
 //            fee: 25,
 //        },
 //        {
-//            countries: ['Denmark', 'Ireland', 'Italy', 'Luxembourg', 'Poland', 'Portugal', 'Spain'],
+//            countries: ["Denmark", "Ireland", "Italy", "Luxembourg", "Poland", "Portugal", "Spain"],
 //            fee: 12,
 //        },
 //        {
-//            countries: ['Finland', 'Malaysia', 'Philippines', 'Singapore'],
+//            countries: ["Finland", "Malaysia", "Philippines", "Singapore"],
 //            fee: 20,
 //        },
 //        {
-//            countries: ['Sweden'],
+//            countries: ["Sweden"],
 //            fee: 10,
 //        },
 //    ]
@@ -320,43 +320,43 @@ func klektSellerPrice(price: Double) -> Double {
 //    }
 //
 //    switch (sellerInfo.countryName) {
-//        case 'UK':
-//        case 'US (mainland)':
-//        case 'US (Alaska, Hawaii)':
-//        case 'Germany':
+//        case "UK":
+//        case "US (mainland)":
+//        case "US (Alaska, Hawaii)":
+//        case "Germany":
 //            sellerFee = 5
 //            break
-//        case 'Austria':
-//        case 'Belgium':
+//        case "Austria":
+//        case "Belgium":
 //            sellerFee = 6
 //            break
-//        case 'Sweden':
-//        case 'Netherlands':
+//        case "Sweden":
+//        case "Netherlands":
 //            sellerFee = 10
 //            break
-//        case 'Ireland':
-//        case 'Luxembourg':
-//        case 'France':
+//        case "Ireland":
+//        case "Luxembourg":
+//        case "France":
 //            sellerFee = 12
 //            break
-//        case 'Italy':
-//        case 'Finland':
-//        case 'Portugal':
-//        case 'Spain':
-//        case 'Denmark':
-//        case 'Malaysia':
-//        case 'Philippines':
-//        case 'Singapore':
+//        case "Italy":
+//        case "Finland":
+//        case "Portugal":
+//        case "Spain":
+//        case "Denmark":
+//        case "Malaysia":
+//        case "Philippines":
+//        case "Singapore":
 //            sellerFee = 20
-//        case 'Bulgaria':
+//        case "Bulgaria":
 //            sellerFee = 24
-//        case 'China':
+//        case "China":
 //            sellerFee = 25
 //            break
 //        default:
 //            break
 //    }
-//    sellerFee = convert(sellerFee, 'USD', currencyCode, false, exchangeRates)
+//    sellerFee = convert(sellerFee, "USD", currencyCode, false, exchangeRates)
 //    const commissionFee = (price * sellerInfo.goat.commissionPercentage) / 100
 //    const totalSellingFee = sellerFee + commissionFee
 //    const sellerPrice = price - totalSellingFee
@@ -373,13 +373,13 @@ func klektSellerPrice(price: Double) -> Double {
 //    exchangeRates?: ExchangeRates
 // ): number => {
 //    let shippingFee: { fee: number; currency: CurrencyInternal } = { fee: 40, currency: USD }
-//    if (sellerInfo.countryName === 'US (mainland)') {
+//    if (sellerInfo.countryName === "US (mainland)") {
 //        shippingFee = { fee: 12, currency: USD }
-//    } else if (sellerInfo.countryName === 'US (Alaska, Hawaii)') {
+//    } else if (sellerInfo.countryName === "US (Alaska, Hawaii)") {
 //        shippingFee = { fee: 15, currency: USD }
-//    } else if (sellerInfo.countryName === 'UK') {
+//    } else if (sellerInfo.countryName === "UK") {
 //        shippingFee = { fee: 13, currency: GBP }
-//    } else if (sellerInfo.countryName === 'China') {
+//    } else if (sellerInfo.countryName === "China") {
 //        shippingFee = { fee: 25, currency: USD }
 //    }
 //    const shipping = convert(
@@ -404,50 +404,34 @@ func restocksSellerPrice(price: Double,
     return round(price - sellerFeeInTargetCurrency)
 }
 
-//
-// const restocksBuyerPrice = (
-//    price: number,
-//    currencyCode: CurrencyCode,
-//    sellerInfo: SellerInfo,
-//    exchangeRates?: ExchangeRates
-// ): number | undefined => {
-//    // USD, EUR, GBP
-//    let fee: number
-//    const fees = [15, 10, 10]
-//    switch (currencyCode) {
-//        case 'USD':
-//            fee = fees[0]
-//            break
-//        case 'EUR':
-//            fee = fees[1]
-//            break
-//        case 'GBP':
-//            fee = fees[2]
-//            break
-//    }
-//    let notSupportedCountries: CountryName[] = [
-//        'Iceland',
-//        'Indonesia',
-//        'Malaysia',
-//        'Philippines',
-//        'South Korea',
-//        'Taiwan',
-//        'Thailand',
-//        'Vietnam',
-//    ]
-//    if (
-//        notSupportedCountries
-//            .map((country) => {
-//                let c: string = country
-//                return c
-//            })
-//            .includes(sellerInfo.countryName)
-//    ) {
-//        return undefined
-//    } else {
-//        return price + fee
-//    }
-// }
+func restocksBuyerPrice(price: Double,
+                        currencyCode: Currency.CurrencyCode,
+                        feeCalculation: CopDeckSettings.FeeCalculation,
+                        exchangeRates: ExchangeRates?) -> Double? {
+    let fees: [Double] = [15, 10, 10]
+    var fee: Double = fees[0]
+    switch currencyCode {
+    case .usd:
+        fee = fees[0]
+    case .eur:
+        fee = fees[1]
+    case .gbp:
+        fee = fees[2]
+    default:
+        fee = fees[0]
+    }
+    let notSupportedCountries: [String] = ["Iceland",
+                                           "Indonesia",
+                                           "Malaysia",
+                                           "Philippines",
+                                           "South Korea",
+                                           "Taiwan",
+                                           "Thailand",
+                                           "Vietnam"]
+    let isNotSupportedCountry = notSupportedCountries.contains(where: { feeCalculation.country.name == $0 })
+    return isNotSupportedCountry ? nil : price + fee
+}
+
 //
 // const calculatePrice = (
 //    storeId: StoreId,
@@ -457,7 +441,7 @@ func restocksSellerPrice(price: Double,
 //    exchangeRates?: ExchangeRates
 // ): StoreInventoryItem => {
 //    switch (storeId) {
-//        case 'stockx':
+//        case "stockx":
 //            if (inventoryItem.lowestAsk) {
 //                inventoryItem.lowestAskWithSellerFees = stockxSellerPrice(
 //                    inventoryItem.lowestAsk,
@@ -487,7 +471,7 @@ func restocksSellerPrice(price: Double,
 //                )
 //            }
 //            break
-//        case 'klekt':
+//        case "klekt":
 //            if (inventoryItem.lowestAsk) {
 //                inventoryItem.lowestAskWithSellerFees = klektSellerPrice(inventoryItem.lowestAsk)
 //                inventoryItem.lowestAskWithBuyerFees = klektBuyerPrice(
@@ -505,7 +489,7 @@ func restocksSellerPrice(price: Double,
 //                )
 //            }
 //            break
-//        case 'goat':
+//        case "goat":
 //            if (inventoryItem.lowestAsk) {
 //                inventoryItem.lowestAskWithSellerFees = goatSellerPrice(
 //                    inventoryItem.lowestAsk,
@@ -535,7 +519,7 @@ func restocksSellerPrice(price: Double,
 //                )
 //            }
 //            break
-//        case 'restocks':
+//        case "restocks":
 //            if (inventoryItem.lowestAsk) {
 //                inventoryItem.lowestAskWithSellerFees = restocksSellerPrice(
 //                    inventoryItem.lowestAsk,
